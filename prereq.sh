8f826d57b68d4d15fac64aabe6b13d60921fd678
fdcipriano <162881579+fdcipriano@users.noreply.github.com>
2024-03-10 21:50:40 +0800

    Update prereq.sh

--- prereq.sh
+++ prereq.sh
@@ -1,4 +1,5 @@
 #Install singularity prerequisites
+sudo apt-get update
 sudo apt-get install -y autoconf  automake cryptsetup fuse \
      fuse2fs git libfuse-dev libglib2.0-dev libseccomp-dev \
 	 libtool pkg-config runc squashfs-tools squashfs-tools-ng uidmap wget zlib1g-dev
