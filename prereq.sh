f8b9b4b54e43bf61ebf2a9376b8a262b502c9a13
fdcipriano <162881579+fdcipriano@users.noreply.github.com>
2024-03-10 18:54:42 +0800

    Add files via upload

---
+++ prereq.sh
@@ -0,0 +1,4 @@
+#Install singularity prerequisites
+sudo apt-get install -y autoconf  automake cryptsetup fuse \
+     fuse2fs git libfuse-dev libglib2.0-dev libseccomp-dev \
+	 libtool pkg-config runc squashfs-tools squashfs-tools-ng uidmap wget zlib1g-dev
