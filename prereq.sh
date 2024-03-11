20bee996383421983555f54ca45bf2457405c2b9
fdcipriano <162881579+fdcipriano@users.noreply.github.com>
2024-03-11 12:43:01 +0800

    Update prereq.sh

--- prereq.sh
+++ prereq.sh
@@ -1,2 +1,2 @@
 sudo apt-get update
-sudo apt-get install -y autoconf automake cryptsetup fuse fuse2fs git libfuse-dev libglib2.0-dev libseccomp-dev libtool pkg-config runc squashfs-tools squashfs-tools-ng uidmap zlib1g-dev wget
+sudo DEBIAN_FRONTEND=noninteractive apt install -y libseccomp-dev
