7b38a63f6c49a726292a41df2f860f4c551af7ed
fdcipriano <162881579+fdcipriano@users.noreply.github.com>
2024-03-13 10:17:38 +0800

    Add files via upload

--- prereq.sh
+++ prereq.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-apt-get update
-apt install -y libseccomp-dev
+# Demo install package on Ubuntu 20.04 via On-Configured Section
+apt update
+apt install -y autoconf automake cryptsetup fuse fuse2fs git libfuse-dev \
+libglib2.0-dev libseccomp-dev libtool pkg-config runc squashfs-tools \
+squashfs-tools-ng uidmap wget zlib1g-dev
